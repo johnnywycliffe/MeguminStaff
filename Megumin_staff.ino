a2208815b3cddf435c02ffac4842b734d68412ba
johnnywycliffe <johnnywycliffe@gmail.com>
2024-01-13 20:58:03 -0500

    Refactored to have second ring, sped up updates, added configurability

--- Megumin_staff.ino
+++ Megumin_staff.ino
@@ -1,99 +1,142 @@
 #include <Adafruit_CircuitPlayground.h>
 // do NOT include the standard NeoPixel library
 
-#define NEOPIX_PIN    A2
-#define NUM_PIXELS    5
+#define NEOPIX_PIN      A2
+#define NUM_PIXELS      16
+#define ONBOARD_PIXELS  10
+#define EXPLOSION_LOOPS 500
+#define PREFIRE_LOOPS   1024
+#define REST_MAX        128
+#define TAP_HARDNESS    126
+#define BRIGHTNESS      128
 
-Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(NUM_PIXELS, NEOPIX_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_CPlay_NeoPixel strip = Adafruit_CPlay_NeoPixel(NUM_PIXELS, NEOPIX_PIN, NEO_GRBW + NEO_KHZ800);
 
-int cycle = 0;
-bool fired;
+bool fired, up, req_update;
+uint8_t stage = REST_MAX - 1;
+const uint8_t TOTAL = NUM_PIXELS + ONBOARD_PIXELS;
+const uint8_t ARR_SIZE = TOTAL * 3;
+uint8_t Colors[ARR_SIZE];
+uint8_t cycle = 0;
 
 void setup() {
-  CircuitPlayground.begin(255);
+  CircuitPlayground.begin(BRIGHTNESS);
+  // Accelorometer
   CircuitPlayground.setAccelRange(LIS3DH_RANGE_2_G);
-  CircuitPlayground.setAccelTap(1, 125);
+  CircuitPlayground.setAccelTap(1, TAP_HARDNESS);
   attachInterrupt(digitalPinToInterrupt(CPLAY_LIS3DH_INTERRUPT), Accel, FALLING);
+  //Extenal Strip
   strip.begin();
+  req_update = true;
+  // debugging
+  //Serial.begin(11520);
+  //while (!Serial){;}
+  //Serial.println("Ready!");
 }
 
 void loop() {
   if (fired){
     Explosion();
     fired = false;
+  } else {
+    Rest();
   }
+}
+
+void Display(){
+  if (!req_update){
+    return;
+  }
+  CircuitPlayground.clearPixels();
+  strip.clear();
+  for (int pixel = 0; pixel < ONBOARD_PIXELS; pixel++){
+    CircuitPlayground.setPixelColor(pixel, Colors[pixel*3], Colors[pixel*3+1], Colors[pixel*3+2]);
+  }
+  for (int pixel = 0; pixel < NUM_PIXELS; pixel++){
+    strip.setPixelColor(pixel, Colors[(pixel+ONBOARD_PIXELS)*3], Colors[(pixel+ONBOARD_PIXELS)*3+1], Colors[(pixel+ONBOARD_PIXELS)*3+2]);
+  }
+  strip.show();
+  for (int cl = 0; cl < ARR_SIZE; cl++){
+    Colors[cl] = 0;
+  }
+  req_update = false;
+}
+
+void Rest(){
+  if (stage < REST_MAX){
+    stage++;
+    req_update = true;
+  }
+  for(int i = 0; i < TOTAL; i++){
+    Colors[i*3] = stage;
+  }
+  Display();
+  delay(250);
 }
 
 void Accel(){
   fired = true;
-}
-
-void Rest(){
-  CircuitPlayground.clearPixels();
-  for (int i = 0; i < 10; i++){
-    CircuitPlayground.setPixelColor(i,64,0,0);
-  }
+  stage = 0;
 }
 
 void Explosion(){
-  for(int j = 0; j < 70; j++){
-    cycle++;
-    cycle = cycle%10;
-    PreFire(cycle);
+  for (int j = 0; j < PREFIRE_LOOPS; j++){
+    PreFire();
   }
-  for (int j = 0; j < 300; j++){
+  for (int j = 0; j < EXPLOSION_LOOPS; j++){
     Fire();
   }
-  Reset();
 }
 
-void PreFire(int pos){
-  CircuitPlayground.clearPixels();
-  for (int i = 0; i < 10; i++){
-    CircuitPlayground.setPixelColor(cycle%10,0,0,0);
-    CircuitPlayground.setPixelColor((cycle+1)%10,0,0,32);
-    CircuitPlayground.setPixelColor((cycle+2)%10,0,0,64);
-    CircuitPlayground.setPixelColor((cycle+3)%10,0,0,128);
-    CircuitPlayground.setPixelColor((cycle+4)%10,0,0,192);
-    CircuitPlayground.setPixelColor((cycle+5)%10,0,0,255);
-    CircuitPlayground.setPixelColor((cycle+6)%10,64,0,255);
-    CircuitPlayground.setPixelColor((cycle+7)%10,128,0,255);
-    CircuitPlayground.setPixelColor((cycle+8)%10,192,0,255);
-    CircuitPlayground.setPixelColor((cycle+9)%10,255,0,255);
+void PreFire(){
+  //Pulse
+  if (up){
+    cycle++;
+  } else {
+    cycle = cycle - 1;
   }
+  //Reverse the pulse
+  if (cycle == 255){
+    up = false;
+  } else if (cycle == 0){
+    up = true;
+  }
+  //Set colors
+  for (int i = 0; i < TOTAL; i++){
+    Colors[i*3+2] = 255;
+    Colors[i*3] = cycle;
+  }
+  req_update = true;
+  Display();
+  delay(1);
 }
 
 void Fire(){
-  for (int i = 0; i < 10; i++){
-    int choice = random(10);
-    switch(choice){
-      case 0:
-      case 1:
-      case 7:
-      case 8:
-      case 9:
-        CircuitPlayground.setPixelColor(i,255,0,0);
-        break;
-      case 2:
-        CircuitPlayground.setPixelColor(i,255,255,255);
-        break;
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-        CircuitPlayground.setPixelColor(i,255,255,0);
-        break;
+  for (int i = 0; i < TOTAL; i++){
+    Colors[i*3] = 255;
+    uint8_t choice = random(256);
+    if (choice > 128){
+      Colors[i*3+1] = 255;
     }
-    delay(1);
+    if (choice > 230){
+      Colors[i*3+2] = 255;
+    }
   }
+  req_update = true;
+  Display();
+  delay(1);
 }
 
-void Reset(){
-  for (int color = 0; color < 64; color++){
-    CircuitPlayground.clearPixels();
-    for (int i = 0; i < 10; i++){
-      CircuitPlayground.setPixelColor(i,color,0,0);
-    }
-    delay(1000);
+void PrintArray(){
+  Serial.println("===============ARRAY=================");
+  Serial.println("\t| RED\t| GRN\t| BLU\t"); 
+  for (int z = 0; z < TOTAL; z++){
+    Serial.print(z);
+    Serial.print("\t| ");
+    Serial.print(Colors[z*3]);
+    Serial.print("\t| ");
+    Serial.print(Colors[(z*3)+1]);
+    Serial.print("\t| ");
+    Serial.println(Colors[(z*3)+2]);
   }
 }
